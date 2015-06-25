e88d3dff2e73ef9b10c3772bacc3e7d61453ad97
Scott Fleckenstein <nullstyle@gmail.com>
2015-06-25 08:15:38 -0700

    Remove last reference to macro

--- src/github.com/nullstyle/go-codegen/cmd/go-codegen/main.go
+++ src/github.com/nullstyle/go-codegen/cmd/go-codegen/main.go
@@ -4,7 +4,7 @@ import (
 	"log"
 	"os"
 
-	macro "github.com/nullstyle/go-codegen"
+	codegen "github.com/nullstyle/go-codegen"
 )
 
 func main() {
@@ -14,7 +14,7 @@ func main() {
 	}
 
 	for _, arg := range args {
-		err := macro.Process(arg)
+		err := codegen.Process(arg)
 
 		if err != nil {
 			log.Fatalln(err)
